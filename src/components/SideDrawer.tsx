50bd44f281e7914b966e0a8b6c4ead0b67101fb9
Mathias Wakgari <mathiaswakgari12@gmail.com>
2023-10-28 15:33:13 +0300

    Refactor: move catergories

--- src/components/SideDrawer.tsx
+++ src/components/SideDrawer.tsx
@@ -8,113 +8,16 @@ import {
   DrawerOverlay,
   HStack,
   Text,
-  useDisclosure,
 } from "@chakra-ui/react";
 import logo from "../assets/logos/logo-no-background.svg";
-import { AiOutlineCode, AiOutlineHome, AiOutlineMenu } from "react-icons/ai";
-import {
-  BiMusic,
-  BiLogoReact,
-  BiPodcast,
-  BiCameraMovie,
-  BiFootball,
-  BiBitcoin,
-} from "react-icons/bi";
-import {
-  IoGameControllerOutline,
-  IoSchoolOutline,
-  IoShirtOutline,
-} from "react-icons/io5";
-import { PiTelevisionSimpleBold } from "react-icons/pi";
-import { FaRegFaceGrinHearts } from "react-icons/fa6";
-import { MdOutlineTheaterComedy } from "react-icons/md";
-import { IoIosFitness } from "react-icons/io";
+import { AiOutlineMenu } from "react-icons/ai";
 import color from "../color";
+import categories from "../utils/categories";
 
 interface Props {
   isOpen: boolean;
   onClose: () => void;
 }
-
-const categories = [
-  {
-    name: "New",
-    icon: <AiOutlineHome color={color.businessNameColor} fontSize={"30px"} />,
-  },
-  {
-    name: "Coding",
-    icon: <AiOutlineCode color={color.businessNameColor} fontSize={"30px"} />,
-  },
-  {
-    name: "ReactJS",
-    icon: <BiLogoReact color={color.businessNameColor} fontSize={"30px"} />,
-  },
-  {
-    name: "Music",
-    icon: <BiMusic color={color.businessNameColor} fontSize={"30px"} />,
-  },
-  {
-    name: "Education",
-    icon: <IoSchoolOutline color={color.businessNameColor} fontSize={"30px"} />,
-  },
-  {
-    name: "Podcast",
-    icon: <BiPodcast color={color.businessNameColor} fontSize={"30px"} />,
-  },
-  {
-    name: "Movie",
-    icon: <BiCameraMovie color={color.businessNameColor} fontSize={"30px"} />,
-  },
-  {
-    name: "Gaming",
-    icon: (
-      <IoGameControllerOutline
-        color={color.businessNameColor}
-        fontSize={"30px"}
-      />
-    ),
-  },
-  {
-    name: "Live",
-    icon: (
-      <PiTelevisionSimpleBold
-        color={color.businessNameColor}
-        fontSize={"30px"}
-      />
-    ),
-  },
-  {
-    name: "Sport",
-    icon: <BiFootball color={color.businessNameColor} fontSize={"30px"} />,
-  },
-  {
-    name: "Fashion",
-    icon: <IoShirtOutline color={color.businessNameColor} fontSize={"30px"} />,
-  },
-  {
-    name: "Beauty",
-    icon: (
-      <FaRegFaceGrinHearts color={color.businessNameColor} fontSize={"30px"} />
-    ),
-  },
-  {
-    name: "Comedy",
-    icon: (
-      <MdOutlineTheaterComedy
-        color={color.businessNameColor}
-        fontSize={"30px"}
-      />
-    ),
-  },
-  {
-    name: "Gym",
-    icon: <IoIosFitness color={color.businessNameColor} fontSize={"30px"} />,
-  },
-  {
-    name: "Crypto",
-    icon: <BiBitcoin color={color.businessNameColor} fontSize={"30px"} />,
-  },
-];
 
 const SideDrawer = ({ isOpen, onClose }: Props) => {
   return (
