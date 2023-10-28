dad948572eefd596a23d5ad0046e96f5562d651a
Mathias Wakgari <mathiaswakgari12@gmail.com>
2023-10-28 11:47:56 +0300

    Build NavBar

---
+++ src/color.ts
@@ -0,0 +1,8 @@
+const color = {
+  businessNameColor: "#096A2E",
+  sloganColor: "#3C3C3C",
+  iconColor: "#398378",
+  tertiaryColor: "#096A2E",
+  baseColor: "#d1e0d7",
+};
+export default color;
---
+++ src/components/NavBar.tsx
@@ -0,0 +1,37 @@
+import { Box, HStack, Image } from "@chakra-ui/react";
+import { AiOutlineMenu } from "react-icons/ai";
+import logo from "../assets/logos/logo-no-background.svg";
+import color from "../color";
+
+const NavBar = () => {
+  return (
+    <HStack
+      height={"50px"}
+      width={"full"}
+      paddingX={2}
+      borderBottom="1px"
+      borderBottomColor={color.iconColor}
+      position={"fixed"}
+    >
+      <Box
+        borderRadius={"full"}
+        boxSize={"35px"}
+        cursor={"pointer"}
+        display={"flex"}
+        justifyContent={"center"}
+        alignItems={"center"}
+        _hover={{
+          backgroundColor: color.baseColor,
+          transitionDuration: "400ms",
+        }}
+      >
+        <AiOutlineMenu color={color.businessNameColor} fontSize={"25px"} />
+      </Box>
+      <Box>
+        <Image boxSize={"200px"} src={logo} objectFit={"fill"} />
+      </Box>
+    </HStack>
+  );
+};
+
+export default NavBar;
