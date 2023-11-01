5fcfceaa3b602380a8c6a41e3d6ace6992ec8dd9
Mathias Wakgari <mathiaswakgari12@gmail.com>
2023-11-01 11:20:51 +0300

    Fetch Channel detail

--- src/pages/VideoPage.tsx
+++ src/pages/VideoPage.tsx
@@ -4,9 +4,30 @@ import api_client from "../services/api_client";
 
 import { Video } from "../hooks/useVideos";
 import VideoPlayer from "../components/VideoPlayer";
+import { Avatar, Text, VStack } from "@chakra-ui/react";
 
 interface FetchVideo {
   items: Video[];
+}
+interface FetchVChannel {
+  items: Channel[];
+}
+
+interface Channel {
+  snippet: {
+    thumbnails: {
+      default: {
+        url: string;
+      };
+      high: {
+        url: string;
+      };
+      medium: {
+        url: string;
+      };
+    };
+    title: string;
+  };
 }
 
 const VideoPage = () => {
@@ -21,8 +42,26 @@ const VideoPage = () => {
         },
       }),
   });
+  const { data: channel } = useQuery({
+    queryKey: ["channels", data?.data.items[0].snippet.channelId],
+    queryFn: () =>
+      api_client.get<FetchVChannel>("channels", {
+        params: {
+          part: "snippet,statistics",
+          id: data?.data.items[0].snippet.channelId,
+        },
+      }),
+  });
 
-  return <VideoPlayer id={id!} />;
+  console.log(channel);
+
+  const video = data?.data.items[0];
+
+  return (
+    <VStack w={"100%"}>
+      <VideoPlayer id={id!} />
+    </VStack>
+  );
 };
 
 export default VideoPage;
