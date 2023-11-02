b69cf8af7edeac8ddf13012a670d8a6c58173c58
Mathias Wakgari <mathiaswakgari12@gmail.com>
2023-11-02 14:35:11 +0300

    Changed UI font

--- src/main.tsx
+++ src/main.tsx
@@ -1,27 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import { ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
 import { RouterProvider } from "react-router-dom";
 import router from "./routes.tsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 const queryClient = new QueryClient();
-// const theme = extendTheme({
-//   styles: {
-//     global: (props: any) => ({
-//       body: {
-//         bg: mode("#ebf7f0", "#141405")(props),
-//         color: mode("#141405", "#fafaeb")(props),
-//       },
-//     }),
-//   },
-// });
+
+const theme = extendTheme({
+  fonts: {
+    body: "Roboto",
+  },
+});
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <QueryClientProvider client={queryClient}>
         <RouterProvider router={router}></RouterProvider>
         <ReactQueryDevtools />
