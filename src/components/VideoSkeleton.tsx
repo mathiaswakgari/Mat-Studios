1b41729cf8c5383f3c6d4c9922afc0daf69c79e1
Mathias Wakgari <mathiaswakgari12@gmail.com>
2023-11-13 18:41:40 +0300

    made skeleton size responsive

--- src/components/VideoSkeleton.tsx
+++ src/components/VideoSkeleton.tsx
@@ -1,4 +1,4 @@
-import { Box, Skeleton, SkeletonText } from "@chakra-ui/react";
+import { Skeleton, SkeletonText, Box } from "@chakra-ui/react";
 import color from "../color";
 
 const VideoSkeleton = () => {
@@ -8,8 +8,8 @@ const VideoSkeleton = () => {
         startColor={color.bgColor}
         endColor={color.sloganColor}
         borderRadius={"2xl"}
-        height={180}
-        width={320}
+        height={{ base: 150, sm: 120, md: 150 }}
+        width={{ base: 280, sm: 220, md: 280 }}
       ></Skeleton>
       <SkeletonText
         startColor={color.bgColor}
